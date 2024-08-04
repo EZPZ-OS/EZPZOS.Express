43f35eba2231c89ac59053e7e8aed70438fa70f1
sirius <q512102932@outlook.com>
2024-08-04 22:13:38 +1000

    Update SQL_SERVER_CREATE.sql
    Added Voucher, Cuisine, UserVoucher, Order, OrderItem, Customisation

--- database/SQL_SERVER_CREATE.sql
+++ database/SQL_SERVER_CREATE.sql
@@ -114,6 +114,8 @@ ALTER TABLE  [dbo].[UserRole]
 ADD CONSTRAINT [PK_UserRole] PRIMARY KEY CLUSTERED  ([Id] ASC)
 WITH ( FILLFACTOR = 70, IGNORE_DUP_KEY = OFF)
 
+
+-- UserRole - User Constraint
 IF NOT EXISTS (SELECT 1 FROM sys.objects where name='Relation_UserRole_User' and type='F')
 ALTER TABLE [dbo].[UserRole] WITH NOCHECK
       ADD CONSTRAINT [Relation_UserRole_User] FOREIGN KEY
@@ -121,6 +123,7 @@ ALTER TABLE [dbo].[UserRole] WITH NOCHECK
           REFERENCES [dbo].[User]
           ( [Id] )
 
+-- UserRole - Role Constraint
 IF NOT EXISTS (SELECT 1 FROM sys.objects where name='Relation_UserRole_Role' and type='F')
 ALTER TABLE [dbo].[UserRole] WITH NOCHECK
       ADD CONSTRAINT [Relation_UserRole_Role] FOREIGN KEY
@@ -133,7 +136,7 @@ Create Table [dbo].[Event](
 	[EventCode] int NOT NULL,
 	[EventTime] datetime NOT NULL,
 	[ParentId] UNIQUEIDENTIFIER NOT NULL,
-	[ParentTable] NVARCHAR(255) NOT NULL,
+	[ParentTable] NVARCHAR(50) NOT NULL,
 	[IsDeleted] BIT NOT NULL,
 	[CreatedTimestamp] datetime NULL,
 	[CreatedUserId] UNIQUEIDENTIFIER NULL,
@@ -170,6 +173,251 @@ WITH ( FILLFACTOR = 70, IGNORE_DUP_KEY = OFF)
 
 CREATE NONCLUSTERED INDEX [IX_OTPs_Mobile] ON [dbo].[OTP] ([Mobile] ASC)
 WITH (FILLFACTOR = 50, IGNORE_DUP_KEY = OFF);
+
+
+Create Table [dbo].[Voucher](
+	[Id] UNIQUEIDENTIFIER NOT NULL,
+	[ParentId] UNIQUEIDENTIFIER NOT NULL,
+	[ParentTable] NVARCHAR(50) NOT NULL,
+	[Code] NVARCHAR(255) NOT NULL,
+	[Name] NVARCHAR(255) NOT NULL,
+	[Description]  NVARCHAR(MAX) NULL,
+	[Price] Float NOT NULL,
+	[Discount] Float NOT NULL,
+	[Type] NVARCHAR(50) NOT NULL,
+	[ReductionRequired] Float NOT NULL,
+	[EndTimestamp] datetime NOT NULL,
+	[StartTimestamp] datetime NOT NULL,
+	[ValidFromTimestamp] datetime NOT NULL,
+	[ValidToTimestamp] datetime NOT NULL,
+	[EffectiveTimeOfDayFrom] datetime NOT NULL,
+	[EffectiveTimeOfDayTo] datetime NOT NULL,
+	[ValidTimeofDayTo] datetime NOT NULL,
+	[EffectiveDaysOfWeek] NVARCHAR(20) NOT NULL,
+	[IsDeleted] BIT NOT NULL,
+	[CreatedTimestamp] datetime NULL,
+	[CreatedUserId] UNIQUEIDENTIFIER NULL,
+	[UpdatedTimestamp] datetime NULL,
+	[UpdatedUserId] UNIQUEIDENTIFIER NULL
+)
+
+ALTER TABLE  [dbo].[Voucher]
+ADD CONSTRAINT [PK_Voucher] PRIMARY KEY CLUSTERED  ([Id] ASC)
+WITH ( FILLFACTOR = 70, IGNORE_DUP_KEY = OFF)
+
+CREATE NONCLUSTERED INDEX [ParentId] ON [dbo].[Voucher] ([ParentId] ASC, [ParentTable] ASC, [Price] ASC)
+WITH ( FILLFACTOR = 50, IGNORE_DUP_KEY = OFF)
+
+CREATE NONCLUSTERED INDEX [Code] ON [dbo].[Voucher] ([Code] ASC, [ParentTable] ASC, [Price] ASC)
+WITH ( FILLFACTOR = 50, IGNORE_DUP_KEY = OFF)
+
+
+
+Create Table [dbo].[Cuisine](
+	[Id] UNIQUEIDENTIFIER NOT NULL,
+	[Name] NVARCHAR(255) NOT NULL,
+	[Description]  NVARCHAR(MAX) NULL,
+	[Price] Float NOT NULL,
+	[Catetory] NVARCHAR(50) NOT NULL,
+	[Images] NVARCHAR(MAX) NULL,
+	[IsAvailable] BIT NOT NULL,
+	[EstimatedTime] datetime NULL,
+	[ParentId] UNIQUEIDENTIFIER NULL,
+	[ParentTable] NVARCHAR(50) NULL,
+	[IsDeleted] BIT NOT NULL,
+	[CreatedTimestamp] datetime NULL,
+	[CreatedUserId] UNIQUEIDENTIFIER NULL,
+	[UpdatedTimestamp] datetime NULL,
+	[UpdatedUserId] UNIQUEIDENTIFIER NULL
+)
+
+ALTER TABLE  [dbo].[Cuisine]
+ADD CONSTRAINT [PK_Cuisine] PRIMARY KEY CLUSTERED  ([Id] ASC)
+WITH ( FILLFACTOR = 70, IGNORE_DUP_KEY = OFF)
+
+CREATE NONCLUSTERED INDEX [Name] ON [dbo].[Cuisine] ([Name] ASC, [Price] ASC, [IsAvailable] ASC)
+WITH ( FILLFACTOR = 50, IGNORE_DUP_KEY = OFF)
+
+CREATE NONCLUSTERED INDEX [Catetory] ON [dbo].[Cuisine] ([Catetory] ASC, [Price] ASC, [IsAvailable] ASC)
+WITH ( FILLFACTOR = 50, IGNORE_DUP_KEY = OFF)
+
+CREATE NONCLUSTERED INDEX [EstimatedTime] ON [dbo].[Cuisine] ([EstimatedTime] ASC, [Price] ASC, [IsAvailable] ASC)
+WITH ( FILLFACTOR = 50, IGNORE_DUP_KEY = OFF)
+
+CREATE NONCLUSTERED INDEX [ParentId] ON [dbo].[Cuisine] ([ParentId] ASC, [ParentTable] ASC, [Price] ASC, [IsAvailable] ASC)
+WITH ( FILLFACTOR = 50, IGNORE_DUP_KEY = OFF)
+
+
+Create Table [dbo].[UserVoucher](
+	[Id] UNIQUEIDENTIFIER NOT NULL,
+	[UserId] UNIQUEIDENTIFIER NOT NULL,
+	[VoucherId] UNIQUEIDENTIFIER NOT NULL,
+	[OriginQuantity] Float NOT NULL,
+	[ConsumedQuantity] Float NOT NULL,
+	[IssuedTimestamp] datetime NULL,
+	[ExpiryTimestamp] datetime NULL,
+	[IsDeleted] BIT NOT NULL,
+	[CreatedTimestamp] datetime NULL,
+	[CreatedUserId] UNIQUEIDENTIFIER NULL,
+	[UpdatedTimestamp] datetime NULL,
+	[UpdatedUserId] UNIQUEIDENTIFIER NULL
+)
+
+ALTER TABLE  [dbo].[UserVoucher]
+ADD CONSTRAINT [PK_UserVoucher] PRIMARY KEY CLUSTERED  ([Id] ASC)
+WITH ( FILLFACTOR = 70, IGNORE_DUP_KEY = OFF)
+
+
+CREATE NONCLUSTERED INDEX [UserId] ON [dbo].[UserVoucher] ([UserId] ASC, [IssuedTimestamp] ASC, [ExpiryTimestamp] ASC)
+WITH ( FILLFACTOR = 50, IGNORE_DUP_KEY = OFF)
+
+CREATE NONCLUSTERED INDEX [VoucherId] ON [dbo].[UserVoucher] ([VoucherId] ASC, [IssuedTimestamp] ASC, [ExpiryTimestamp] ASC)
+WITH ( FILLFACTOR = 50, IGNORE_DUP_KEY = OFF)
+
+CREATE NONCLUSTERED INDEX [OriginQuantity] ON [dbo].[UserVoucher] ([OriginQuantity] ASC, [ConsumedQuantity] ASC, [IssuedTimestamp] ASC, [ExpiryTimestamp] ASC)
+WITH ( FILLFACTOR = 50, IGNORE_DUP_KEY = OFF)
+
+-- UserVoucher - User Constraint
+IF NOT EXISTS (SELECT 1 FROM sys.objects where name='Relation_UserVoucher_User' and type='F')
+ALTER TABLE [dbo].[UserVoucher] WITH NOCHECK
+      ADD CONSTRAINT [Relation_UserVoucher_User] FOREIGN KEY
+          ( [UserId] )
+          REFERENCES [dbo].[User]
+          ( [Id] )
+
+-- UserVoucher - Voucher Constraint
+IF NOT EXISTS (SELECT 1 FROM sys.objects where name='Relation_UserVoucher_Voucher' and type='F')
+ALTER TABLE [dbo].[UserVoucher] WITH NOCHECK
+      ADD CONSTRAINT [Relation_UserVoucher_Voucher] FOREIGN KEY
+          ( [VoucherId] )
+          REFERENCES [dbo].[Voucher]
+          ( [Id] )
+
+
+Create Table [dbo].[Order](
+	[Id] UNIQUEIDENTIFIER NOT NULL,
+	[UserId] UNIQUEIDENTIFIER NOT NULL,
+	[UserVoucherId] UNIQUEIDENTIFIER NOT NULL,
+	[TableNumber] NVARCHAR(10) NOT NULL,
+	[PaymentOption] Int NOT NULL,
+	[CompletedTimestamp] datetime NULL,
+	[PickupTimestamp] datetime NULL,
+	[IsPaid] BIT NOT NULL,
+	[BranchId] UNIQUEIDENTIFIER NULL,
+	[IsDeleted] BIT NOT NULL,
+	[CreatedTimestamp] datetime NULL,
+	[CreatedUserId] UNIQUEIDENTIFIER NULL,
+	[UpdatedTimestamp] datetime NULL,
+	[UpdatedUserId] UNIQUEIDENTIFIER NULL
+)
+
+ALTER TABLE  [dbo].[Order]
+ADD CONSTRAINT [PK_Order] PRIMARY KEY CLUSTERED  ([Id] ASC)
+WITH ( FILLFACTOR = 70, IGNORE_DUP_KEY = OFF)
+
+
+CREATE NONCLUSTERED INDEX [UserId] ON [dbo].[Order] ([UserId] ASC, [CompletedTimestamp] ASC, [IsPaid] ASC)
+WITH ( FILLFACTOR = 50, IGNORE_DUP_KEY = OFF)
+
+CREATE NONCLUSTERED INDEX [UserVoucherId] ON [dbo].[Order] ([UserVoucherId] ASC, [CompletedTimestamp] ASC, [IsPaid] ASC)
+WITH ( FILLFACTOR = 50, IGNORE_DUP_KEY = OFF)
+
+CREATE NONCLUSTERED INDEX [PaymentOption] ON [dbo].[Order] ([PaymentOption] ASC, [CompletedTimestamp] ASC, [IsPaid] ASC)
+WITH ( FILLFACTOR = 50, IGNORE_DUP_KEY = OFF)
+
+CREATE NONCLUSTERED INDEX [BranchId] ON [dbo].[Order] ([BranchId] ASC, [CompletedTimestamp] ASC, [IsPaid] ASC)
+WITH ( FILLFACTOR = 50, IGNORE_DUP_KEY = OFF)
+
+CREATE NONCLUSTERED INDEX [TableNumber] ON [dbo].[Order] ([TableNumber] ASC, [CompletedTimestamp] ASC, [IsPaid] ASC)
+WITH ( FILLFACTOR = 50, IGNORE_DUP_KEY = OFF)
+
+
+-- Order - User Constraint
+IF NOT EXISTS (SELECT 1 FROM sys.objects where name='Relation_Order_User' and type='F')
+ALTER TABLE [dbo].[Order] WITH NOCHECK
+      ADD CONSTRAINT [Relation_Order_User] FOREIGN KEY
+          ( [UserId] )
+          REFERENCES [dbo].[User]
+          ( [Id] )
+
+-- Order - UserVoucher Constraint
+IF NOT EXISTS (SELECT 1 FROM sys.objects where name='Relation_Order_UserVoucher' and type='F')
+ALTER TABLE [dbo].[Order] WITH NOCHECK
+      ADD CONSTRAINT [Relation_Order_UserVoucher] FOREIGN KEY
+          ( [UserVoucherId] )
+          REFERENCES [dbo].[UserVoucher]
+          ( [Id] )
+
+-- //TODO Add Order - Branch Constrant
+
+
+Create Table [dbo].[OrderItem](
+	[Id] UNIQUEIDENTIFIER NOT NULL,
+	[Quantity] int NOT NULL,
+	[OrderItemType] Int NOT NULL,
+	[CuisineId] UNIQUEIDENTIFIER NOT NULL,
+	[OrderId] UNIQUEIDENTIFIER NOT NULL,
+	[Comments] NVARCHAR(MAX) NOT NULL,
+	[IsDeleted] BIT NOT NULL,
+	[CreatedTimestamp] datetime NULL,
+	[CreatedUserId] UNIQUEIDENTIFIER NULL,
+	[UpdatedTimestamp] datetime NULL,
+	[UpdatedUserId] UNIQUEIDENTIFIER NULL
+)
+
+ALTER TABLE  [dbo].[OrderItem]
+ADD CONSTRAINT [PK_OrderItem] PRIMARY KEY CLUSTERED  ([Id] ASC)
+WITH ( FILLFACTOR = 70, IGNORE_DUP_KEY = OFF)
+
+CREATE NONCLUSTERED INDEX [CuisineId] ON [dbo].[OrderItem] ([CuisineId] ASC, [Quantity] ASC)
+WITH ( FILLFACTOR = 50, IGNORE_DUP_KEY = OFF)
+
+CREATE NONCLUSTERED INDEX [OrderId] ON [dbo].[OrderItem] ([OrderId] ASC, [Quantity] ASC)
+WITH ( FILLFACTOR = 50, IGNORE_DUP_KEY = OFF)
+
+CREATE NONCLUSTERED INDEX [OrderItemType] ON [dbo].[OrderItem] ([OrderItemType] ASC, [Quantity] ASC)
+WITH ( FILLFACTOR = 50, IGNORE_DUP_KEY = OFF)
+
+
+-- OrderItem - Order Constraints
+IF NOT EXISTS (SELECT 1 FROM sys.objects where name='Relation_OrderItem_Order' and type='F')
+ALTER TABLE [dbo].[OrderItem] WITH NOCHECK
+      ADD CONSTRAINT [Relation_OrderItem_Order] FOREIGN KEY
+          ( [OrderId] )
+          REFERENCES [dbo].[Order]
+          ( [Id] )
+
+-- OrderItem - Cuisine Constraints
+IF NOT EXISTS (SELECT 1 FROM sys.objects where name='Relation_OrderItem_Cuisine' and type='F')
+ALTER TABLE [dbo].[OrderItem] WITH NOCHECK
+      ADD CONSTRAINT [Relation_OrderItem_Cuisine] FOREIGN KEY
+          ( [CuisineId] )
+          REFERENCES [dbo].[Cuisine]
+          ( [Id] )
+
+
+Create Table [dbo].[Customisation](
+	[Id] UNIQUEIDENTIFIER NOT NULL,
+	[ParentId] UNIQUEIDENTIFIER NOT NULL,
+	[ParentTable] NVARCHAR(50) NULL,
+	[Name] NVARCHAR(255) NOT NULL,
+	[Value] NVARCHAR(MAX) NOT NULL,
+	[IsDeleted] BIT NOT NULL,
+	[CreatedTimestamp] datetime NULL,
+	[CreatedUserId] UNIQUEIDENTIFIER NULL,
+	[UpdatedTimestamp] datetime NULL,
+	[UpdatedUserId] UNIQUEIDENTIFIER NULL
+)
+
+ALTER TABLE  [dbo].[Customisation]
+ADD CONSTRAINT [PK_Customisation] PRIMARY KEY CLUSTERED  ([Id] ASC)
+WITH ( FILLFACTOR = 70, IGNORE_DUP_KEY = OFF)
+
+CREATE NONCLUSTERED INDEX [ParentId] ON [dbo].[Customisation] ([ParentId] ASC)
+WITH ( FILLFACTOR = 50, IGNORE_DUP_KEY = OFF)
+
+CREATE NONCLUSTERED INDEX [Name] ON [dbo].[Customisation] ([Name] ASC)
+WITH ( FILLFACTOR = 50, IGNORE_DUP_KEY = OFF)
 
 End;
 go
