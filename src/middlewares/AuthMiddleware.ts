7953d42ff8dd50019710d170f2f9a9dbee89a1e4
Linfei Fu <linfei.fu.2803@gmail.com>
2024-09-04 20:36:15 +1000

    updated verifyToken middleware

--- src/middlewares/AuthMiddleware.ts
+++ src/middlewares/AuthMiddleware.ts
@@ -1,29 +1,33 @@
 import { Request, Response, NextFunction } from "express";
 import { DefaultJWTSecretKey } from "ezpzos.core";
-
-const jwt = require('jsonwebtoken');
+import Jwt from "jsonwebtoken";
 
 // Middleware function to check if the authToken is expired
-const verifyToken = (req:Request, res:Response, next:NextFunction) => {
-    const authHeader = req.headers['authorization'];
-    const token = authHeader && authHeader.split(' ')[1]; // Extract token from "Bearer <token>"
+const verifyToken = (req: Request, res: Response, next: NextFunction) => {
+	const authHeader = req.headers["authorization"];
+	const token = authHeader && authHeader.split(" ")[1]; // Extract token from "Bearer <token>"
 
-    if (!token) {
-        return res.status(401).json({ message: 'Access Denied: No Token Provided' });
-    }
+	if (!token) {
+		return res.status(401).json({ message: "Access Denied: No Token Provided" });
+	}
 
-    try {
-        // Decode and verify the token
-        const decoded = jwt.verify(token, DefaultJWTSecretKey);
-
-        // Check if the token is expired
-        const currentTime = Math.floor(Date.now() / 1000);
-        if (decoded.exp < currentTime) {
-            return res.status(401).json({ message: 'Access Denied: Token Expired' });
-        }
-        next(); // Proceed to the next middleware or route handler
-    } catch (err) {
-        return res.status(400).json({ message: 'Invalid Token' });
-    }
+	try {
+		// Decode and verify the token
+		const decoded = Jwt.verify(token, DefaultJWTSecretKey);
+        
+		// Check if the token is a JwtPayload and has an expiration time
+		if (typeof decoded === "object" && "exp" in decoded) {
+			// Check if the token is expired
+			const currentTime = Math.floor(Date.now() / 1000);
+			if (decoded.exp && decoded.exp < currentTime) {
+				return res.status(401).json({ message: "Access Denied: Token Expired" });
+			}
+		} else {
+			return res.status(400).json({ message: "Invalid Token" });
+		}
+		next(); // Proceed to the next middleware or route handler
+	} catch (err) {
+		return res.status(400).json({ message: "Invalid Token" });
+	}
 };
 export default verifyToken;
--- src/routes/Auth.ts
+++ src/routes/Auth.ts
@@ -1,13 +1,9 @@
 import express, { Router } from "express";
 import { mobileLogin, signup } from "../controllers/AuthController";
-import verifyToken from "../middlewares/AuthMiddleware";
 
 const router: Router = express.Router();
 
 router.post("/signup", signup);
 router.post("/login", mobileLogin);
-router.get("/validate-token", verifyToken, (req, res) => {
-    // If the token is valid, the middleware will pass control here
-	res.status(200).json({ message: "Token is valid" });
-});
+
 export default router;
