453cb39939610587060939383c114c0a86b268b0
Linfei Fu <linfei.fu.2803@gmail.com>
2024-09-21 17:40:08 +1000

    Add Prisma and added example usage of prisma with new testing route, controller and service functions.

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -39,3 +39,41 @@ export const UpdateUser = async (req: UpdateUserRequest, res: Response) => {
 		return res.status(500).send("An unexpected error occurred during user update. Please try again later.");
 	}
 };
+
+// Test prisma update
+//TODO: Remove this function or UpdateUser function after testing and confirming prisma adoption
+export const UpdateUserTest = async (req: Request, res: Response) => {
+	const userUpdateObject = req.body;
+
+	// Extract user ID from the request parameters
+	const userId = req.params.id;
+
+	// Check if userUpdates.Id is provided
+	if (!userId) {
+		logger.Log("UpdateUser", "User ID is missing", LogLevel.WARN);
+		return res.status(400).send({ message: "User ID is required" });
+	}
+
+	try {
+		// Use the UserService to update the user
+		const user = await UserService.updateUserTest(userId, userUpdateObject);
+
+		logger.Log("UpdateUser", "User successfully updated", LogLevel.INFO);
+		return res.status(200).send({ result:true, user, message: "User successfully updated" });
+	} catch (error) {
+		logger.Log("UpdateUser", `Error during user update: ${error}`, LogLevel.ERROR);
+		return res.status(500).send("An unexpected error occurred during user update. Please try again later.");
+	}
+};
+
+// Test prisma get
+//TODO: Remove this function after testing and confirming prisma adoption
+export const GetUsers = async (req: Request, res: Response) => {
+	try {
+		const users = await UserService.getUsers();
+		return res.status(200).send({ result:true, users, message: "Users fetched successfully." });
+	} catch (error) {
+		logger.Log("GetUsers", `Error fetching users: ${error}`, LogLevel.ERROR);
+		return res.status(500).send("An unexpected error occurred fetching users. Please try again later.");
+	}
+};
--- src/routes/private/User.ts
+++ src/routes/private/User.ts
@@ -1,8 +1,12 @@
 import { Router } from "express";
-import { UpdateUser } from "../../controllers/UserController";
+import { GetUsers, UpdateUser, UpdateUserTest } from "../../controllers/UserController";
 
 const router: Router = Router();
 
 router.put("/update/:id", UpdateUser)
+
+//TODO: Remove these 2 route after testing and confirming prisma adoption
+router.put("/updateTest/:id", UpdateUserTest)
+router.get("", GetUsers)
 
 export default router;
