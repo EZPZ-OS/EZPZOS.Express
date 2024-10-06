1d9dda12048f629983b00dba65597522140808ef
LinfeiFu <linfei.fu.2803@gmail.com>
2024-10-06 22:17:02 +1000

    created user Admin in database, added adminLogin function (#22)

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { mobileLogin, signup, sendOtp, verifyOtp } from "../controllers/AuthController";
+import { mobileLogin, signup, sendOtp, verifyOtp, adminLogin } from "../controllers/AuthController";
 import AuthMiddleware from "../middlewares/AuthMiddleware";
 import BookingRouter from "./private/Booking";
 import UserRouter from "./private/User";
@@ -8,6 +8,7 @@ import MenuRouter from "./private/Menu"
 /* -- Public Router -- */
 const publicRouter: Router = Router();
 publicRouter.post("/login", mobileLogin);
+publicRouter.post("/login/admin", adminLogin);
 publicRouter.post("/signup", signup);
 publicRouter.post("/send-otp", sendOtp);
 publicRouter.post("/verify-otp", verifyOtp);
